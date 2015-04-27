57d2ba839c403382c76013058da30faae84600aa
JimmyLiu <jimmyliu1994@hotmail.com>
2015-04-27 22:19:42 +0800

    外观改进

--- chemapp/protected/views/site/admin.php
+++ chemapp/protected/views/site/admin.php
@@ -1,66 +1,16 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
+<?php 
+Yii::app()->getClientScript()->registerCssFile(Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('zii.widgets.assets')).'/gridview'.'/styles.css');
+?>
 
 <h1>系统管理</h1>
 <style type="text/css" id="grid-view">
-.grid-view-loading
-{
-	background:url(loading.gif) no-repeat;
-}
-
-.grid-view
-{
-	padding: 15px 0;
-}
-
-.grid-view table.items
-{
-	background: white;
-	border-collapse: collapse;
-	width: 100%;
-	border: 1px #D0E3EF solid;
-}
-
-.grid-view table.items th, .grid-view table.items td
-{
-	font-size: 0.9em;
-	border: 1px white solid;
-	padding: 0.3em;
-}
-
 .grid-view table.items th
 {
-	color: white;
-	background: url("css/bg.gif") repeat-x scroll left top white;
-	text-align: center;
+	font-size: 100%;
+	border: 1px white solid;
+	padding: 0.5em 0.3em;
 }
-
-.grid-view table.items th a
-{
-	color: #EEE;
-	font-weight: bold;
-	text-decoration: none;
-}
-
-.grid-view table.items th a:hover
-{
-	color: #FFF;
-}
-
-.grid-view table.items tr.even
-{
-	background: #F8F8F8;
-}
-
-.grid-view table.items tr.odd
-{
-	background: #E5F1F4;
-}
-
-.grid-view table.items tr:hover
-{
-	background: #ECFBD4;
-}
-
 </style>
 <div class="narrowPage grid-view" style="font-size: 1.5em">
 	<table class="items">
